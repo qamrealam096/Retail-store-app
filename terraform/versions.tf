44404b111ff264426ce17b3fbd83d0cb74d21b47
Qamre Alam <qamre.alam.uk096@gmail.com>
2026-08-25 14:01:36 -0700

    modified

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -38,10 +38,10 @@ provider "aws" {
 }
 
 provider "helm" {
-  kubernetes {
+  kubernetes = {
     host                   = module.retail_app_eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.retail_app_eks.cluster_certificate_authority_data)
-    exec {
+    exec = {
       api_version = "client.authentication.k8s.io/v1beta1"
       command     = "aws"
       args        = ["eks", "get-token", "--cluster-name", module.retail_app_eks.cluster_name]
